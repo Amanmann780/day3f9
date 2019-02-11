a541ef1a2a97c790ff8bb749ad05fb899fc34cca
MacStudent <macstudent@126-iMac18.local>
2019-02-11 18:40:39 -0500

    Initial Commit

---
+++ day3f9/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  day3f9
+//
+//  Created by MacStudent on 2019-02-11.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ day3f9/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  day3f9
+//
+//  Created by MacStudent on 2019-02-11.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
